527ae2c0f69e0c6cf618b2b637b3293786317523
keisukessu <149043402+keisukessu@users.noreply.github.com>
2026-05-16 18:06:25 +0900

    UserDishResponseの削除

--- Mystery-cook/backend/app/schemas/dish.py
+++ Mystery-cook/backend/app/schemas/dish.py
@@ -9,13 +9,7 @@ class UserDishCreate(BaseModel):
     dish_id: uuid.UUID
 
 
-class UserDishResponse(BaseModel):
-    id: uuid.UUID
-    user_id: uuid.UUID
-    dish_id: uuid.UUID
-    cooked_at: datetime
 
-    model_config = {"from_attributes": True}
 
 
 class UserDishListResponse(BaseModel):
